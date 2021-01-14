a779d04fcc6f9f9fdb3eaad8439c9c160c5c54f0
Aye Daniel A <aadaniel108@gmail.com>
2021-01-14 12:57:40 +0100

    login validations and testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   include SessionsHelper
+  before_action :logged_in_user, only: [:edit, :update]
+
   def index
     @users = User.all
   end
@@ -30,7 +32,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-    # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
     render 'edit'
     end
@@ -41,5 +44,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
