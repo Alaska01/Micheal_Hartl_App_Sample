2e6fa726f50f7daeccb785a00d92b8029598c569
Aye Daniel A <aadaniel108@gmail.com>
2021-01-16 17:49:21 +0100

    change username and password config env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,11 +72,14 @@ Rails.application.configure do
     :address  => 'smtp.sendgrid.net',
     :port  => '587',
     :authentication => :plain,
-    :user_name  => ENV['SENDGRID_USERNAME'],
-    :password  => ENV['SENDGRID_PASSWORD'],
+    user_name:      'apikey',
+    password:       ENV['SENDGRID_API_KEY']
     :domain  => 'heroku.com',
     :enable_starttls_auto => true
   }
+
+  # :user_name  => ENV['SENDGRID_USERNAME'],
+    # :password  => ENV['SENDGRID_PASSWORD'],
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
