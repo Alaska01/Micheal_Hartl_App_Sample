310c86c793ab21c261095d3d5bc6dbde5773caa0
Aye Daniel A <aadaniel108@gmail.com>
2021-01-16 16:06:21 +0100

    configure email in production and email activation details

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
-    before_save { self.email = email.downcase }
+    attr_accessor :remember_token, :activation_token, :reset_token
+    before_save :downcase_email
     before_create :create_activation_digest
 
     validates :name, presence: true, length: { maximum: 50 }
@@ -27,9 +27,31 @@ class User < ApplicationRecord
     end
 
     # Returns true if the given token matches the digest.
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    def authenticated?(attribute, token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
+    end
+
+   # Activates an account.
+    def activate
+        update_columns(activated: true, activated_at: Time.zone.now)
+    end
+
+    # Sends activation email.
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
+    end
+
+    # Sets the password reset attributes.
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    end
+    
+    # Sends password reset email.
+    def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
     end
 
     # Forgets a user.
