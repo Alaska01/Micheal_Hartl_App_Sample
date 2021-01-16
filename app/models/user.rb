0db27b9b3ff1d776dfb257b62220e5e99206c5ad
Aye Daniel A <aadaniel108@gmail.com>
2021-01-16 21:48:08 +0100

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,6 +48,12 @@ class User < ApplicationRecord
         self.reset_token = User.new_token
         update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
     end
+
+    # Returns true if a password reset has expired.
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+    end
+
     
     # Sends password reset email.
     def send_password_reset_email
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'evening-falls-84957.herokuapp.com'
+  host = 'warm-waters-29914.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address  => 'smtp.sendgrid.net',
@@ -77,11 +77,6 @@ Rails.application.configure do
     :domain  => 'heroku.com',
     :enable_starttls_auto => true
   }
-
-  # :user_name =>     'apikey',
-  # :password =>      ENV['SENDGRID_API_KEY']
-
- 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
