255e4dd8e903ba36906648e2bbadbb33e286e2d2
Aye Daniel A <aadaniel108@gmail.com>
2021-01-18 14:45:02 +0100

    fix mixup with current_user?(user)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,6 +18,11 @@ module SessionsHelper
         end
     end
 
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+        user && user == current_user
+    end
+
     def remember(user)
         user.remember
         cookies.permanent.encrypted[:user_id] = user.id
